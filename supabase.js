5f7172fb644841e3c4dd49b13405e179d1d7decb
Mateusz Chruściel <108098349+MatthevC@users.noreply.github.com>
2026-08-29 08:30:20 +0200

    Add files via upload

---
+++ supabase.js
@@ -0,0 +1,4 @@
+// UZUPEŁNIJ DANYMI Z SUPABASE (Settings -> API)
+const SUPABASE_URL = "YOUR_SUPABASE_URL";
+const SUPABASE_ANON_KEY = "YOUR_SUPABASE_ANON_KEY";
+const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
