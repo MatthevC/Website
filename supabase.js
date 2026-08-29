f44f7a54037f3cd0e31fd114203d5ebb4eb2376b
Mateusz Chruściel <108098349+MatthevC@users.noreply.github.com>
2026-08-29 08:44:32 +0200

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -2,3 +2,6 @@
 const SUPABASE_URL = "https://hbfgiecmyzgldgbbgwds.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_DQHt3NmJnRXkNIaM-Fmj7Q_5YT7yyA9";
 const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+window.supabaseClient = supabaseClient;
+
+console.log("Supabase połączony:", supabaseClient);
