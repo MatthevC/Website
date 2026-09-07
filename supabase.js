c4e1656edfbea95068322cf0c58d319a4b19bbc5
Mateusz Chruściel <108098349+MatthevC@users.noreply.github.com>
2026-09-07 18:29:53 +0200

    Delete supabase.js

--- supabase.js
+++
@@ -1,9 +0,0 @@
-// UZUPEŁNIJ DANYMI Z SUPABASE (Settings -> API)
-const SUPABASE_URL = "https://hbfgiecmyzgldgbbgwds.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_DQHt3NmJnRXkNIaM-Fmj7Q_5YT7yyA9";
-const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
-  auth: { persistSession: true, autoRefreshToken: true, detectSessionInUrl: true }
-});
-window.supabaseClient = supabaseClient;
-
-console.log("Supabase połączony:", supabaseClient);
