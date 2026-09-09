16d4b81691e2bccc1950a1cd2608a716415128df
Mateusz Chruściel <108098349+MatthevC@users.noreply.github.com>
2026-09-09 10:42:09 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ function highlightSidebarTarget(target) {
   void visual.offsetWidth;
   visual.classList.add('sidebar-nav-highlight');
 
-  setTimeout(() => { visual.classList.remove('sidebar-nav-highlight'); }, 4000);
+  setTimeout(() => { visual.classList.remove('sidebar-nav-highlight'); }, 4700);
 }
 
 function activateSidebarLink(links, link, sections, targetId, progress) {
